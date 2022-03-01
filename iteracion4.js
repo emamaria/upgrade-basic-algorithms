f3537b8eeb7caf0821a74a5fd30b87d66f47f93a
emamaria <emascript@gmail.com>
2022-03-01 14:30:15 +0100

    chore: add iteracion4

---
+++ iteracion4.js
@@ -0,0 +1,49 @@
+
+
+
+//1.1
+
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+
+console.log(avengers[0]);
+
+
+//1.2
+
+avengers[0] = "IRONMAN";
+
+
+
+//1.3
+
+console.log(avengers.length);
+
+
+//1.4
+
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
+
+rickAndMortyCharacters.push("Morty", "Summer");
+
+console.log(rickAndMortyCharacters[rickAndMortyCharacters.length - 1]);
+
+
+//1.5
+
+const rickAndMortyCharacters1 = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+
+rickAndMortyCharacters1.pop()
+
+console.log(rickAndMortyCharacters1[0], rickAndMortyCharacters1[rickAndMortyCharacters.length - 1]);
+
+
+//1.6
+
+
+const rickAndMortyCharacters2 = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+
+rickAndMortyCharacters2.splice(1, 1)
+
+console.log(rickAndMortyCharacters2);
+
+
