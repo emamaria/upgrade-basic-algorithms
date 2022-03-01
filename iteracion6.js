03b669ab501b742a2514cc57716171a8b54576f5
emamaria <emascript@gmail.com>
2022-03-01 15:31:36 +0100

    chore: add iteracion6

---
+++ iteracion6.js
@@ -0,0 +1,32 @@
+//1.1
+
+for(let i = 0; i <= 9 ; i++){
+    console.log(i);
+}
+
+
+//1.2
+
+
+for(let i = 0; i <= 9 ; i++){
+
+   if(i%2 == 0){
+    console.log(i);
+   }
+
+}
+    
+
+//1.3
+
+for(let i = 0; i < 10; i++){
+
+    if(i < 9){
+    console.log('Intentando dormir');
+    }
+   
+    if(i == 9){
+    console.log('Dormido!');  
+    }
+
+}
